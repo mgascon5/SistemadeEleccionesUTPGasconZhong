3363560b153d5086ef6c6e1f92036f933c1cd37b
mgascon5 <113870997+mgascon5@users.noreply.github.com>
2022-09-18 21:54:33 -0500

    actualizado

--- app/src/main/java/com/example/sistemadeeleccionesutpgasconzhong/MainActivity.java
+++ app/src/main/java/com/example/sistemadeeleccionesutpgasconzhong/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.sistemadeeleccionesutpgasconzhong;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button boton1;
+    private EditText tx1;
+    private TextView titulo1,titulo2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
